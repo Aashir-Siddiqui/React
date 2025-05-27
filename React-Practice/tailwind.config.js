8b06b545f2a5db46f4ac7f104fe4091068cf27b6
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-05-28 01:42:18 +0500

    Update todo app

--- React-Practice/tailwind.config.js
+++ React-Practice/tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/project/Todo/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          600: '#7c3aed',
+          700: '#6d28d9',
+          800: '#5b21b6',
+        },
+      },
+    },
   },
   plugins: [],
-}
+  darkMode: 'class',
+};
