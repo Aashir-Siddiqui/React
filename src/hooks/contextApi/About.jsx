e44f7d77c2e907a39438632b5565c7a9b7c90861
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-06-13 03:23:11 +0500

    Custom hooks practice

--- src/hooks/contextApi/About.jsx
+++ src/hooks/contextApi/About.jsx
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react'
+import React, { use } from 'react'
 import { BioContext } from '.'
 
 export const About = () => {
 
-    const {myInstitute, myBatch, myCourse} = useContext(BioContext)
+    const { myInstitute, myBatch, myCourse } = use(BioContext)
 
-  return (
-    <div>
-      <h1>I am learning {myCourse} from {myInstitute} and i am {myBatch} student.</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>I am learning {myCourse} from {myInstitute} and i am {myBatch} student.</h1>
+        </div>
+    )
 }
